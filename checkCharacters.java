5faa89712dd1eca092f73fb1abc37e832f5517fd
Eisuke Takahashi <eisuke.takahashi@outlook.com>
2015-02-20 10:50:14 +0900

    Update checkCharacters.java
    SJISのうち、第一水準、第二水準、IBM拡張文字の場合はtrueを返す
    　　　　　　　　第一水準のうち？,〓、　NEC選定IBM拡張文字、それ以外はfalseを返す

--- checkCharacters.java
+++ checkCharacters.java
@@ -1,30 +1,39 @@
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 
-//---------テスト用　ここから---------------
+//---------テスト用コード　ここから---------------
 class TestcheckCharacters{
+	//arg[0]で指定したテキストファイルを読み込み、checkCharactersメソッドへ渡し、結果を出力する
 	public static void main(String args[]){
-		boolean result = checkCharacters("あいうえお順");	//テスト対象文字列を入れる箇所
-		System.out.println(result);
+		String thisLine;
+
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(args[0]));
+			while((thisLine = br.readLine())!= null){
+				boolean result = checkCharacters(thisLine);
+				System.out.println(thisLine + " => " + result);
+			}
+		} catch(IOException e) {
+			System.err.println(e);
+		}
 	}
-//---------テスト用　ここまで---------------
-	
+//---------テスト用コード　ここまで---------------
 	
 	
 
-
+//--------------メソッド本体-------------------
 public static boolean checkCharacters(String target){
 	char[] charArray = target.toCharArray();
 	int targetChar;
 	
 	for(char ch : charArray){
 		try{
-			byte[] bArray = String.valueOf(ch).getBytes("Windows-31J");
+			byte[] bArray = String.valueOf(ch).getBytes("SJIS");	//SJISのバイトコード取得
  
 			if (bArray.length == 1) {
-				//1バイト文字
+				//1バイト文字を16進数へ
 				targetChar = bArray[0] & 0xFF;
 	 		} else {
-				//2バイト文字
+				//2バイト文字を16進数へ
 				targetChar = ((bArray[0] & 0xFF) << 8) | (bArray[1] & 0xFF);
 			}
 			
@@ -34,12 +43,14 @@ public static boolean checkCharacters(String target){
 		
 		
 		if((0x8140 <= targetChar) && (targetChar <= 0x9872)){ // 第一水準かチェック
-				if(( 0x8148 == targetChar) || ( 0x81AC == targetChar)){ return false; } // 第一水準のなかで"？","〓"の場合は、falseを返す
-			/* 処理なし	*/
-		} else if ((0x989F <= targetChar) && (targetChar <= 0xFC4B)){ // 第二水準かチェック
-			/* 処理なし	*/
-		} else if ((0xFA40 <= targetChar) && (targetChar <= 0xFC40B)){ // IBM拡張文字かチェック
-			/* 処理なし	*/
+				if((0x8148 == targetChar) || (0x81AC == targetChar)){ return false; } // 第一水準のなかで"？","〓"の場合は、falseを返す
+			// 処理なし
+		} 
+		else if ((0x989F <= targetChar) && (targetChar <= 0xEAA4)){  // 第二水準かチェック
+			//処理なし
+		}	
+		else if ((0xED40 <= targetChar) && (targetChar <= 0xEEFD)){  // IBM拡張文字かチェック
+			// 処理なし 		NEC選定IBM拡張文字は((0xFA40 <= targetChar) && (targetChar <= 0xFC40F))の範囲のため、チェック必要なし
 		}
 		else {
 			return false;
@@ -48,6 +59,7 @@ public static boolean checkCharacters(String target){
 	}
 	return true;
 }
+//--------------メソッド本体ここまで----------------
 
 
 
@@ -55,5 +67,4 @@ public static boolean checkCharacters(String target){
 
 
 
-
-}// <-- テスト用
+}// <-- テスト用コード
