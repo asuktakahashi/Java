7376c95ff49244d083a8f54bce2f8b85cf3098f0
Eisuke Takahashi <eisuke.takahashi@outlook.com>
2015-09-08 22:58:08 +0900

    Update EditTexFilesRecursively.java

--- EditTexFilesRecursively.java
+++ EditTexFilesRecursively.java
@@ -1,12 +1,42 @@
-class EditTextFilesRecursively{
-  public static void main(String args[]){
-   
-   
-    try{
+import java.io.*;
+
+public class EditTextFilesRecursively{
+  public void readDirectory(File dir){
+    File files[] = dir.listFiles();
+    if( files = null){ return; }
     
-    } catch(IOException e) {
+    for(File file: fles){
+      if(!file.exists())
+        continue;
+      else if(file.isDirectory())
+        readDirectory(file)
+      else if(file.isFile())
+        excute(file);
+    }
+  }
+  
+  public void execute(File file){ 
+    try{
+      
+      BufferedReader br = new BufferedReader(new FileReader(file));
+      BufferedWriter bw = new BufferedWriter(new FileWriter("result.txt"));
+      
+      String line;
+      while((line = br.readLine()) != null){
+        bw.write(line);
+        bw.newLine();
+      }
+      
+      bw.close();
+      br.close();
+    
+    } catch(Exception e) {
       System.err.println(e);
     }
-  
   }
+}
+
+
+public static void main(String args[]){
+  new EditTextFilesRecursively().readDirectory(new File("C:¥workspace¥"));
 }
